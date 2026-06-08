d16625ae33057a84f54bd9439b7ca2c487d259f6
Iyke_hermes <31313901+ogumbaiyke@users.noreply.github.com>
2026-06-08 15:46:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -56,6 +56,11 @@ console.log(numOfMonitors);
 
 
 // Helper Functions
+//Another way to define a function is to use a function expression. 
+//To define a function inside an expression, we can use the function keyword. 
+//In a function expression, the function name is usually omitted. 
+//A function with no name is called an anonymous function. 
+//A function expression is often stored in a variable in order to refer to it. 
 
 function monitorCount(rows, columns) {
   return rows * columns;
