28af748afb16e0e90087d0ccf3c7483e57fa1157
Iyke_hermes <31313901+ogumbaiyke@users.noreply.github.com>
2026-06-08 13:11:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -18,6 +18,16 @@ sayThanks();
 sayThanks();
 
 
+// ----------------------------------------------------------------------------------------------------------
+
+function sayThanks(name) {
+  console.log('Thank you for your purchase, '+ name + '! We appreciate your business.')
+}
+
+sayThanks(`Cole`);
+
+
+
 
 
 
