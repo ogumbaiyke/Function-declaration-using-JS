b860d106c037c65950a1fb1a114d27759b702a7c
Iyke_hermes <31313901+ogumbaiyke@users.noreply.github.com>
2026-06-08 14:25:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,22 @@ console.log(numOfMonitors);
 
 
 
+//-------------------------------------------------------------------------------------------------------------
 
+
+// Helper Functions
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns) * 200;
+}
+
+const totalCost = costOfMonitors(5, 4);
+
+console.log(totalCost)
 
 
 
