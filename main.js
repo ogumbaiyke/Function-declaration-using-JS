0651116aaf181e49403aab709bdaa4f3c21a64c5
Iyke_hermes <31313901+ogumbaiyke@users.noreply.github.com>
2026-06-08 15:43:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -70,6 +70,19 @@ const totalCost = costOfMonitors(5, 4);
 console.log(totalCost)
 
 
+//----------------------------------------------------------------------------------------------------------
+// Function Expressions.
+
+const plantNeedsWater = function(day) {
+  if(day === 'Wednesday'){
+    return true;
+} else{
+    return false
+}
+}
+
+console.log(plantNeedsWater('Tuesday'));
+
 
 
 
