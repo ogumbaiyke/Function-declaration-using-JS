a5a13f8f74ef85c3be5d6a99c33b69177f0e948f
Iyke_hermes <31313901+ogumbaiyke@users.noreply.github.com>
2026-06-08 13:38:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -27,6 +27,15 @@ function sayThanks(name) {
 sayThanks(`Cole`);
 
 
+// -----------------------------------------------------------------------------------------------------------
+
+
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+};
+
 
 
 
